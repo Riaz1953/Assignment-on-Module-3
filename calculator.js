244306500685de934e8628fddeb4cc38a43e738c
Md Riaz <fxkiyan2020@gmail.com>
2026-06-05 22:29:58 +0200

    task 3 complete

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 5;
+
+let Addition = `Addition: ${num1 + num2}`;
+
+console.log(Addition); // Addition: 15
+
+let Subtraction = `Subtraction: ${num1 - num2}`;
+console.log(Subtraction); // Subtraction: 5
+
+let Multiplication = `Multiplication: ${num1 * num2}`;
+console.log(Multiplication); // Multiplication: 50
+
+let Division = `Division: ${num1 / num2}`;
+console.log(Division); // Division: 2
+
+let Modulus = `Modulus: ${num1 % num2}`;
+console.log(Modulus); // Modulus: 0
