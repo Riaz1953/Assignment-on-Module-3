2239b7923c657d1918e1c94af6adc5c38d527f36
Md Riaz <fxkiyan2020@gmail.com>
2026-06-05 22:04:49 +0200

    task 2 complete

--- datatype.js
+++ datatype.js
@@ -2,13 +2,15 @@
 let name = "Md  Riazul Islam";
 let age = 35;
 let isStudent = true;
-let district = "Jamalpur";
+let district = "Noakhali";
 let salary = null;
-let phone = "01318430121";
+let phone = "01727405822";
 
 console.log(typeof name); // string
 console.log(typeof age); // number
 console.log(typeof isStudent); // boolean
 console.log(typeof district); // string
-console.log(typeof salary); // object (null is considered an object in JavaScript)
+
+console.log(typeof salary); // object (type of check করলে null এর type, object  দেখাবে। এটা js এর একটা legacy bug. এটা solve না করার কারন হল, solve করলে পুরাতন কোডে error দেখাবে।
+
 console.log(typeof phone); // string
