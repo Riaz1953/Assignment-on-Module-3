89e6c67a4ab684cd3c2c8a12d1cfe68981646687
Md Riaz <fxkiyan2020@gmail.com>
2026-06-05 22:46:15 +0200

    task 4 complete

---
+++ result.js
@@ -0,0 +1,14 @@
+// Task 4: Pass or Fail Program
+let marks = 79;
+
+if (marks >= 80) {
+  console.log("Your Grade: A+");
+} else if (marks >= 70) {
+  console.log("Your Grade: A");
+} else if (marks >= 60) {
+  console.log("Your Grade: B");
+} else if (marks >= 50) {
+  console.log("Your Grade: C");
+} else {
+  console.log("You are fail");
+}
