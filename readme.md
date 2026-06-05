fc4bb68e2c00a6314ae47aff682915f52fef48c5
Md Riaz <fxkiyan2020@gmail.com>
2026-06-05 21:44:35 +0200

    task one complete

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Module 3 Assignment
+
+JavaScript Fundamentals Practice
+
+Topics:
+
+- Variables
+
+- Data Types
+
+- Operators
+
+- Conditionals
+
+- Loops
---
+++ student.js
@@ -0,0 +1,11 @@
+// Task 1: Student Information
+
+let name = "Md Riazul Islam"; // Name: Md Riazul Islam
+let age = 35; // Age: 35
+let district = "Noakhali"; // District: Noakhali
+let isStudent = true; // Student: true
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age} `);
+console.log(`District: ${district}`);
+console.log(`Student: ${isStudent}`);
